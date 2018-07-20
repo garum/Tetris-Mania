47f0e1c746845261b631190e809a5c0f9eb688df
Valeriu Sabau <valeriusabau29@gmail.com>
2018-07-20 11:53:10 +0300

    Drew cavanas

---
+++ logic.js
@@ -0,0 +1,10 @@
+const cavanas=document.getElementById("tetris-box");
+const context=cavanas.getContext('2d');
+
+context.fillStyle="#000";
+context.fillRect(0,0,cavanas.width,cavanas.height);
+
+function gameLoop()
+{
+    
+}

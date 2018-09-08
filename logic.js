613a9444660e256ae75964ac74e80c906bb9818e
Valeriu Sabau <valeriusabau29@gmail.com>
2018-09-08 18:10:28 +0300

    v1.0 finished

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
-const cavanas=document.getElementById("tetris-box");
-const context=cavanas.getContext('2d');
+const canvas=document.getElementById("tetris-box");
+const context=canvas.getContext('2d');
 
 
 const boardHeight=22;
@@ -8,14 +8,15 @@ const nrPixel=24;
 
 var gameBoard=creatMatrix(boardHeight,boardWeight);
 context.fillStyle="#000";
-context.fillRect(0,0,cavanas.width,cavanas.height);
+context.fillRect(0,0,canvas.width,canvas.height);
 context.scale(nrPixel,nrPixel);
 
 
 const matrixTest=[
-                    [0,1,0],
-                    [1,1,0],
-                    [0,1,0]
+                    [0,0,1,0],
+                    [0,0,1,0],
+                    [0,0,1,0],
+                    [0,0,1,0]
                 ];
 
 function creatMatrix(h,w){
@@ -25,41 +26,44 @@ function creatMatrix(h,w){
         return matrix;
 }
 
-function drawMatrix(matrix,position)
+function drawMatrix(matrix,position,color)
 {
+    pos=position;
     for(var i=0;i<matrix.length;i++)
         for(var j=0;j<matrix[0].length;j++)
         {
-            if(matrix[i][j]==1)
-                 context.fillStyle='red';
             if(matrix[i][j]==10)
                 context.fillStyle='black';
-            if(matrix[i][j]!=0)
-                 context.fillRect(j+position.x,i+position.y,1,1);
+            else if(matrix[i][j]==1)
+                context.fillStyle=player.color;
+            else if(matrix[i][j]!=0){
+                context.fillStyle=matrix[i][j];
+            }
+
+            if(matrix[i][j]!=0) 
+                 context.fillRect(j+pos.x,i+pos.y,1,1);
         }
 }
 
 var player={
      matrix:matrixTest,
-     position:{'x':boardWeight/2,'y':0}
+     position:{'x':boardWeight/2,'y':0},
+     color:'#FF0000'
 };
 
 function checkCollision(player,gameBoard){
     const pMatrix=player.matrix;
     const pos=player.position;
-//  if(pos.y+pMatrix.length>boardHeight || pos.x<0 || pos.x+pMatrix[0].length>boardWeight)
-      // return true
-
 
     for(var i = 0;i < pMatrix.length; i++)
         for(var j = 0; j < pMatrix[i].length; j++)
         {
-           // console.log(pos.y+i, pos.x+j,(pos.y+i>=pMatrix.length || pos.x+j<0 || pos.x+j>=pMatrix[i].length));
+           
             if(pos.y+i>=gameBoard.length || pos.x+j<0 || pos.x+j>=gameBoard[0].length ){
                 if( pMatrix[i][j]==1)
                 return true;
             }else
-                 if(pMatrix[i][j] == 1 && (gameBoard[pos.y+i][pos.x+j] == 1))
+                 if(pMatrix[i][j] == 1 && (gameBoard[pos.y+i][pos.x+j] != 10))
                      return true;
         }
 
@@ -69,14 +73,22 @@ function checkCollision(player,gameBoard){
 function resetPlayer()
 {
     player.position={'x':boardWeight/2,'y':0};
+    key=Math.floor(Math.random()*5);
+    getRandomTetrimon(key);
+  
 }
 
 function playerGravity(){
     player.position.y++;
+    let collided=false
     if(checkCollision(player,gameBoard)){
       player.position.y--;
-      merge(player,gameBoard);
-      resetPlayer();
+      collided=true;
+    }
+
+    if(collided){
+        merge(player,gameBoard);
+        resetPlayer();
     }
 
 }
@@ -94,29 +106,89 @@ function merge(player,gameBoard){
         for(var j=0;j<m[0].length;j++)
         {
             if(m[i][j]==1)
-                gameBoard[pos.y+i][pos.x+j]=1;
+                gameBoard[pos.y+i][pos.x+j]=player.color;
         }
     }
 }
 
+function rotatePlayer(player,direction){
 
+    pMatrix=player.matrix;
+    var h=pMatrix.length;
+    var w=pMatrix[0].length;
+    buffer=creatMatrix(h,w);
+
+    for(let i=0;i<h;i++)
+        for(let j=0;j<w;j++)
+            if(direction==1)
+                buffer[i][j]=pMatrix[j][h-i-1];
+            else
+                 buffer[i][j]=pMatrix[w-j-1][i];
+
+    player.matrix=buffer;
+}
+
+function checkRotation(player,direction,gameBoard){
+
+    var initialPosition=player.position
+    let offset=1;
+    rotatePlayer(player,direction);
+
+    while(checkCollision(player,gameBoard)){
+        player.position.x+=offset;
+        
+        if(offset>0)
+            offset=-offset-1;
+        else offset=-offset+1;
+
+        if(offset > player.matrix.length) {
+            rotatePlayer(player,-direction);
+            player.position=initialPosition;
+            return;
+        }
+    }
+}
+
+function removeRow(gameBoard,row){
+
+    gameBoard[row]=new Array(boardWeight).fill(10);
+    for(let i=row;i>=1;i--){
+        for(let j=0;j<gameBoard[0].length;j++){
+            gameBoard[i][j]=gameBoard[i-1][j];
+
+        }
+    }    
+        
+}
+
+function lineSweep(gameBoard){  
+
+    for(let i=gameBoard.length-1; i>=0;i--){
+        var full=true;
+        for(let j=0;j<gameBoard[0].length;j++) {
+            if(gameBoard[i][j]==10)
+                full=false;
+        }
+
+        if(full){
+            removeRow(gameBoard,i);
+        }
+    }
+            
+}
 var tick=0;
 
 function update(){
-    
-
     tick++;
     if(tick>13){
         // update  movement of the player
         playerGravity();
+        lineSweep(gameBoard);
         tick=0;
     }
-
-    //console.log(player.position,checkCollision(player,gameBoard));
     // Render gameBoard and the player 
-
-   drawMatrix(gameBoard,{'x':0,'y':0}); 
-   drawMatrix(player.matrix,player.position);
+   drawMatrix(gameBoard,{'x':0,'y':0},player.color); 
+   drawMatrix(player.matrix,player.position,player.color);
    window.requestAnimationFrame(update);
 }
 
@@ -127,7 +199,57 @@ document.addEventListener("keydown",event => {
             playerMove(-1);
         if(event.keyCode==39)
             playerMove(1);
+        if(event.keyCode==65)
+            checkRotation(player,1,gameBoard);
+        if(event.keyCode==68)
+            checkRotation(player,-1,gameBoard);
 });
+
+function getRandomTetrimon(key){
+    if(key==0){
+        player.matrix=[
+            [0,0,1,0],
+            [0,0,1,0],
+            [0,0,1,0],
+            [0,0,1,0]
+        ];
+        player.color='#FF0000';
+    }
+    if(key==1){
+        player.matrix=[
+            [0,0,1],
+            [0,0,1],
+            [0,1,1]
+        ];
+        player.color='#EFFF00';
+    }
+    if(key==2){
+        player.matrix=[
+            [0,1,0],
+            [1,1,1],
+            [0,0,0]
+        ];
+        player.color='#009999';
+    }
+    if(key==3){
+        player.matrix=[
+            [0,0,0],
+            [0,1,1],
+            [1,1,0]
+        ];
+        player.color='#89FF00';
+    }
+    if(key==4){
+        player.matrix=[
+           [1,1],
+           [1,1]
+        ];
+        player.color='#6600CC';
+        
+    }
+
+}
+
 
 update();
 
